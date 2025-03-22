c7643c6230b7af4dcea1dfdff56ac477822a8584
Bruno Rodrigues <bruno_rdgs_@hotmail.com>
2025-03-22 18:08:23 -0300

    chore: Criar '.properties' e 'import.sql'

---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,33 @@
+INSERT INTO tb_clientes(nome, cpf, data_nascimento, endereco) VALUES('Bruno Rodrigues', '18557302444', '2000-10-18', 'Rua São José, 570');
+INSERT INTO tb_clientes(nome, cpf, data_nascimento, endereco) VALUES('Ana Elisa', '32725690189', '2002-1-27', 'Rua Líbano , 681');
+INSERT INTO tb_clientes(nome, cpf, data_nascimento, endereco) VALUES('Larissa Rodrigues', '26616978222', '1982-9-23', 'Rua Montenegro, 213');
+INSERT INTO tb_clientes(nome, cpf, data_nascimento, endereco) VALUES('João Silva', '26229026925', '2001-5-25', 'Rua Nicarágua , 508');
+INSERT INTO tb_clientes(nome, cpf, data_nascimento, endereco) VALUES('Fernada Oliveira', '21399675198', '2000-2-15', 'Rua Portugal, 150');
+INSERT INTO tb_clientes(nome, cpf, data_nascimento, endereco) VALUES('Roberta Carvalho', '51925318842', '1986-1-9', 'Rua San Marino, 315');
+INSERT INTO tb_clientes(nome, cpf, data_nascimento, endereco) VALUES('Antonio Miranda', '29311361489', '2000-1-3', 'Rua Turquia , 475');
+INSERT INTO tb_clientes(nome, cpf, data_nascimento, endereco) VALUES('André Silvestre', '58897381495', '1985-1-3', 'Rua Uganda, 287');
+INSERT INTO tb_clientes(nome, cpf, data_nascimento, endereco) VALUES('Leandro Lima', '46633927465', '1995-4-12', 'Rua Itália , 464');
+INSERT INTO tb_clientes(nome, cpf, data_nascimento, endereco) VALUES('Lucas Laranjeira', '20335392468', '1988-12-12', 'Rua Castro Alves, 701');
+INSERT INTO tb_clientes(nome, cpf, data_nascimento, endereco) VALUES('Bianca Fernades', '29400720043', '2001-1-14', 'Rua Rui Barbosa, 315');
+
+
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(8, 'Telefone', '(58)97562-9383', 'Corporativo');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(7, 'Telefone', '(29)97185-9922', 'Profissional');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(10, 'E-mail', 'lucas@gmail.com', 'Pessoal');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(8, 'Telefone', '(83)98942-9862', 'Corporativo');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(1, 'Telefone', '(29)97428-9933', 'Profissional');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(7, 'E-mail', 'antonio@outlook.com', 'Corporativo');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(11, 'Telefone', '(21)94642-9111', 'Pessoal');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(5, 'Telefone', '(14)97903-7115', 'Pessoal');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(7, 'Telefone', '(58)97450-7465', 'Pessoal');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(3, 'E-mail', 'larissa@hotmail.com', 'Corporativo');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(2, 'Telefone', '(64)96676-5396', 'Corporativo');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(9, 'Telefone', '(67)98202-8756', 'Pessoal');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(3, 'Telefone', '(40)97905-7319', 'Pessoal');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(3, 'E-mail', 'larissa@hotmail.com', 'Corporativo');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(10, 'E-mail', 'lucas@outlook.com', 'Corporativo');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(2, 'E-mail', 'ana@outlook.com', 'Corporativo');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(5, 'E-mail', 'fernanda@outlook.com', 'Corporativo');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(10, 'E-mail', 'lucas@hotmail.com', 'Profissional');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(9, 'E-mail', 'leandro@gmail.com', 'Profissional');
+INSERT INTO tb_contatos(cliente id, tipo, valor, observacao) VALUES(7, 'Telefone', '(43)96853-8545', 'Pessoal');
